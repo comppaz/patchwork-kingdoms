0463c4ee5625622a55732f1f97a619d0f314148c
Antonio Maiolo <antonio@maiolo.net>
2022-03-09 18:28:29 +0100

    added new frontend README

--- web/scripts/hashImages.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import imagehash
-
-for idx in range(1, 1001):
-    print(idx)
-
-    hash = imagehash.average_hash(Image.open(
-        f'/Users/antonio/Downloads/highres-visuals-pk/{idx}.png'))
-    print(hash)
