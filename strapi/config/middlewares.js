5b70f31b74df6b1289ded64bc3e53b92e1706d9a
Antonio Maiolo <antonio@maiolo.net>
2022-03-16 12:28:37 +0100

    fixed middleware sec policy

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -1,6 +1,16 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('DO_SPACE_BUCKET')}.fra1.digitaloceanspaces.com`],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
