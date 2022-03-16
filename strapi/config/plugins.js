ad474f8c96bc667b22a18618ed8a97fe5cf7d779
Antonio Maiolo <antonio@maiolo.net>
2022-03-16 12:17:39 +0100

    added do plugin

---
+++ strapi/config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env('DO_SPACE_ACCESS_KEY'),
+        secret: env('DO_SPACE_SECRET_KEY'),
+        endpoint: env('DO_SPACE_ENDPOINT'),
+        space: env('DO_SPACE_BUCKET'),
+        directory: env('DO_SPACE_DIRECTORY'),
+      }
+    },
+  },
+})
