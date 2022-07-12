5e4dafea7c136a7f6f5d88b467a3de486123b375
Antonio Maiolo <antonio@craft-clarity.com>
2022-07-12 12:10:09 +0200

    cleanup

--- web/pages/api/getDonatedETHperPWK.js
+++ web/pages/api/getDonatedETHperPWK.js
@@ -15,14 +15,10 @@ export default async function getDonatedETHperPWK(req, res){
     let url = "https://api.opensea.io/api/v1/events?only_opensea=true&token_id="+tokenId+"&asset_contract_address="+asset_contract_address+"&event_type=successful";
 
     const response = await fetch(url, options);
-
-    console.log(response)
-
     const data = await response.json();
     let totalDonated = initMintPrice;
 
-    console.log(data);
-
+    console.log(`Received a response from OpenSea: ${JSON.stringify(data)}`);
     
     if(data && (data.asset_events.length > 0)){
         data.asset_events.forEach(element => {
