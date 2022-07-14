59a735eb1856b7212b09c642802c39bc4a1fb19f
Simona Maiolo <simona.maiolo95@gmail.com>
2022-07-14 14:44:56 +0200

    added loading view and changed overflow

--- web/pages/nft/[...id].js
+++ web/pages/nft/[...id].js
@@ -12,10 +12,9 @@ import getNftFromJsonBins from '../../lib/getNftFromJsonBins';
 
 const NFT = () => {
   const router = useRouter()
-   const [map, setMap] = useState();
-   const [data, setData] = useState();
-   const mapNode = useRef(null);
-
+  const [data, setData] = useState();
+  const mapNode = useRef(null);
+  const [loading, setLoading] = useState(false);
   const [isDialogOpen, setDialogOpen] = useState(true);
 
  useEffect(async () => {
@@ -27,6 +26,7 @@ const NFT = () => {
    const node = mapNode.current;
    
    if (typeof window === "undefined" || node === null) return;
+   setLoading(true)
    const data = await getNftFromJsonBins(tokenId);
    //const data = await import(`../api/metadata/${tokenId}.json`)
    const features = data['schools'].concat(data['schools_no_data']);
@@ -36,6 +36,7 @@ const NFT = () => {
    data.donatedETH = await getDonatedETHperPWK(tokenId);
   
    setData(data);
+   setLoading(false);
    let colorPalette = data.attributes[0]["value"];
    
    const mapboxMap = new mapboxgl.Map({
@@ -228,34 +229,41 @@ const generatePopup = (map, coordinates, properties) => {
   return (<>
     
     <div className="relative pt-4 bg-white overflow-hidden">
-      {/* mobile screens: open dialog panel from bottom */}
       {!isDialogOpen ? 
       <div>
+        {/* mobile screens: open dialog panel from bottom */}
         <button 
-        onClick={() => {setDialogOpen(true)}} 
-        className='inline-flex items-center visible px-8 py-3 sm:invisible absolute z-10 bottom-10 left-5 bg-white text-gray-600 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'
-        >
-          About 
+          onClick={() => {setDialogOpen(true)}} 
+          className='inline-flex items-center visible px-8 py-3 sm:invisible absolute z-10 bottom-10 left-5 bg-white text-gray-600 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'
+          >
+            About 
+            <span class="inline-flex items-center w-4 h-4">
+              <ChevronUpIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
+            </span>
+        </button>
+        {/* desktop screens: open dialog panel from left side */}
+        <button  
+          onClick={() => {setDialogOpen(true)}} 
+          className='inline-flex items-center visible px-8 py-3 invisible sm:visible absolute z-10 top-10 left-5 w-45 bg-white text-gray-600 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'
+          >
+          About
           <span class="inline-flex items-center w-4 h-4">
-            <ChevronUpIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
-          </span>
-      </button>
-      {/* desktop screens: open dialog panel from left side */}
-      <button  
-        onClick={() => {setDialogOpen(true)}} 
-        className='inline-flex items-center visible px-8 py-3 invisible sm:visible absolute z-10 top-10 left-5 w-45 bg-white text-gray-600 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2'
-        >
-        About
-        <span class="inline-flex items-center w-4 h-4">
-            <ChevronRightIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
-          </span>
-      </button>
+              <ChevronRightIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
+            </span>
+        </button>
       </div> 
       : null
       }
-      <div ref={mapNode} style={{ width: "auto", height: "82vh" }} />
+      {loading ? 
+      <div className='fixed z-10 inset-1/2'>
+        <svg role="status" className="relative mr-2 w-8 h-8 text-gray-400 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+      </div> 
+      : <div ref={mapNode} style={{ width: "auto", height: "82vh" }} />}
     </div>
-    <Slideover data={data} isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen}></Slideover>
+    {loading ? null : <Slideover data={data} isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen}></Slideover>}  
     </>
   )
 }
