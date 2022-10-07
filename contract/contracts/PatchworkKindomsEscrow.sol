58970780ffb0931f00751b3b917a2507e4e6042c
Simona Maiolo <simona.maiolo95@gmail.com>
2022-10-07 16:53:52 +0200

    modified escrow contract

--- contract/contracts/PatchworkKindomsEscrow.sol
+++ contract/contracts/PatchworkKindomsEscrow.sol
@@ -1,20 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
 contract PatchworkKingdomsEscrow {
     IERC721 token;
     uint256 counter;
-    // TODO: so richtig?
-    //address payable public owner;
+    address payable private owner;
 
     struct ERC721Item {
-        address donatorGiver;
+        address giver;
         uint256 item;
         uint256 expiration;
         uint256 price;
@@ -24,9 +19,8 @@ contract PatchworkKingdomsEscrow {
 
     event Deposited(
         uint256 id,
-        uint256 price,
         address tokenAddress,
-        uint256 item
+        uint256 tokenId
     );
     event Donated(
         uint256 id,
@@ -35,65 +29,112 @@ contract PatchworkKingdomsEscrow {
         uint256 item
     );
 
-    constructor(IERC721 _token) {
-        token = _token;
+    constructor(address _address) {
+        token = IERC721(_address);
         counter = 0;
+        owner = payable(msg.sender);
     }
 
     /**
     deposit function puts token in escrow 
     */
     function deposit(
-        uint256 _item,
-        uint256 _expiration,
-        uint256 _price
+        uint256 tokenId,
+        uint256 _expiration
     ) public {
         require(
-            msg.sender == token.ownerOf(_item),
+            msg.sender == token.ownerOf(tokenId),
             "Sender is not the token owner."
         );
+        token.approve(address(this), tokenId);
         // transfer the item to escrow contract
-        token.transferFrom(msg.sender, address(this), _item);
+        token.transferFrom(msg.sender, address(this), tokenId);
         uint256 itemId = counter;
         // update items with current deposited item
         items[itemId] = ERC721Item({
-            donatorGiver: msg.sender,
-            item: _item,
+            giver: msg.sender,
+            item: tokenId,
+            // TODO: alternative for expiration: the calculation of the expiration_date is done in frontend and passed as parameter into this function
             expiration: block.timestamp + _expiration,
-            price: _price
+            price: 0
         });
         counter += 1;
-        emit Deposited(itemId, _price, address(token), _item);
+        emit Deposited(itemId, address(token), tokenId);
+    }
+
+    /**
+    setLastMinPrice function is called by the oracle service after the Deposited event has been triggered
+    @param lastMinPrice: the value the oracle calculates 
+    @param itemId: id of the deposited item
+    */
+    function setLastMinPrice(uint256 lastMinPrice, uint256 itemId) public view {
+        ERC721Item memory item = items[itemId];
+        item.price = lastMinPrice;
+    }
+
+    /**
+    getItem function returns specific item
+    @param itemId: id of the requested item
+    */
+    function getItem(uint256 itemId) public view returns(ERC721Item memory){
+        ERC721Item memory item = items[itemId];
+        return item;
     }
 
     /**
     donation function receives token from escrow by paying the price
     @param itemId: id of an existing item in items
-    @param _price: price the donatorBuyer wants to pay, must be minimum the price set by donatorGiver
     */
-    function donation(uint256 itemId, uint256 _price) public {
+    function donation(uint256 itemId) public payable {
         ERC721Item memory item = items[itemId];
         // check price value
         require(
-            _price > item.price,
+            msg.value > item.price,
             "The price value does not exceed or is equal to the minimum price."
         );
         // check expiration value
+        // TODO: see above and below how to handle expiration
         require(block.timestamp <= item.expiration, "The token is expired.");
         token.transferFrom(address(this), msg.sender, item.item);
-        emit Donated(itemId, _price, address(token), item.item);
+        delete(items[itemId]);
+        emit Donated(itemId, msg.value, address(token), item.item);
     }
 
-    // TODO: should the admin have the right to cancel a deposit?
     /**
-    cancelDeposit function allows the admin to cancel a deposit and transfer back the token to the donatorGiver
-    
+    cancelDeposit function allows the admin to cancel a deposit and transfer back the token to the giver
+    @param itemId: id of an existing item in items
+    */
     function cancelDeposit(uint256 itemId) public {
         ERC721Item memory item = items[itemId];
         require(
             msg.sender == owner,
-            "Trade can only be cancelled by the admin."
+            "Deposit can only be cancelled by the admin."
         );
-        token.transferFrom(address(this), item.donatorGiver, item.item);
-    }*/
+        delete(items[itemId]);
+        token.transferFrom(address(this), item.giver, item.item);
+    }
+
+    /**
+    expiration function allows to return the deposited item back to the giver if the expiration time is exceeded
+    @param itemId: id of an existing item in items
+    */
+    function expiration(uint256 itemId, uint256 _expiration) public {
+        ERC721Item memory item = items[itemId];
+        // TODO: expiration value could be verified with block.timestamp see above: depends on initial decision
+        require(_expiration < item.expiration, "The token is not expired.");
+        delete(items[itemId]);
+        token.transferFrom(address(this), item.giver, item.item);
+    }
+
+    /**
+    withdraw function allows the admin to withdraw the received eths on this address
+    */
+    function withdraw() public {
+        require(
+            msg.sender == owner,
+            "Tokens can only be withdrawn by the admin."
+        );
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Failed to withdraw.");
+    }
 }
