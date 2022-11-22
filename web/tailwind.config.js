32f3e443cb6671d3700b5b66e6842e916aea2377
Antonio Maiolo <antonio@craft-clarity.com>
2022-11-22 10:14:59 +0100

    cleanuo

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
     mode: 'jit',
-    purge: ['./node_modules/flowbite-react/**/*.js', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
