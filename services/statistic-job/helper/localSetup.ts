2f80a2768753b602662a574de9af0417a9b5926c
Antonio Maiolo <antonio@craft-clarity.com>
2023-02-02 11:31:59 +0100

    fix and clean up

--- services/statistic-job/helper/localSetup.ts
+++
@@ -1,7 +0,0 @@
-const handle = require("../index.ts");
-
-// run the following in helper folder with the following cmd: yarn ts-node ./helper/localSetup.ts
-// updating db with the api call results
-handle.handler();
-// updating db with the current rank values for the weekly rankValues
-//handle.weeklyHandler();
