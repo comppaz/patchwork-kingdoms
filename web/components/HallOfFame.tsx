e9cd2463078883243243c6878aeebe1d3bdfde76
Simona Maiolo <simona.maiolo95@gmail.com>
2023-05-31 11:47:21 +0200

    small fixes

--- web/components/HallOfFame.tsx
+++ web/components/HallOfFame.tsx
@@ -41,11 +41,24 @@ export default function HallOfFame({ donators }) {
             </div>
             <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                 {rankedDonators.slice(0, 3).map((don, index) => (
-                    <div className=" w-13/15 px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6" key={index}>
-                        <dt className="text-sm font-medium text-gray-500 truncate">Rank: {index + 1}</dt>
-                        <dd className="text-sm font-medium text-gray-500 truncate">Total Sale: {don.salePrice} ETH</dd>
-                        <dd className="text-sm font-medium text-gray-500 truncate">
-                            Address: {`${don.address.replace(don.address.substr(12, 24), '...')}`}
+                    <div
+                        className=" grid grid-rows-3 grid-flow-col gap-1 w-13/15 px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6"
+                        key={index}>
+                        {index == 0 && <p className="row-span-3 flex justify-center items-center text-2xl">🥇</p>}{' '}
+                        {index == 1 && <p className="row-span-3 flex justify-center items-center text-2xl">🥈</p>}{' '}
+                        {index == 2 && <p className="row-span-3 flex justify-center items-center text-2xl">🥉</p>}{' '}
+                        <dt className="col-span-2 text-sm font-medium text-gray-500 truncate">
+                            <span className="font-bold">Rank: </span>
+                            {index + 1}
+                        </dt>
+                        <dd className="col-span-2 text-sm font-medium text-gray-500 truncate">
+                            {' '}
+                            <span className="font-bold">Total Sale: </span>
+                            {don.salePrice} ETH
+                        </dd>
+                        <dd className="col-span-2 text-sm font-medium text-gray-500 truncate">
+                            <span className="font-bold">Address: </span>
+                            {`${don.address.replace(don.address.substr(12, 24), '...')}`}
                         </dd>
                     </div>
                 ))}
